1c454e4efb9c971e796dcff722f269ca4a42966a
Вероника <v.milchakova@securitycode>
2017-09-15 18:50:15 +0300

    Добавлены обработки ошибок

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,65 +1,59 @@
 import org.apache.commons.cli.*;
 
+import javax.imageio.IIOException;
 import java.io.*;
 
 public class MainActivity {
 
     public static void main(String[] args) throws Exception {
-        Options options = new Options();
-
-        Option copy = new Option("cp", "input", true, "copy file");
-        copy.setRequired(false);
-        options.addOption(copy);
-
-        Option remove = new Option("rm", "remove", true, "remove file");
-        remove.setRequired(false);
-        options.addOption(remove);
-
-        Option move = new Option("mv", "move", true, "move file");
-        move.setRequired(false);
-        options.addOption(move);
-
-        Option create = new Option("f", "createFile", true, "create file");
-        create.setRequired(false);
-        options.addOption(create);
-
 
         if (args.length == 0) {
-            System.out.println("No options found!");
+            optionsNumberErr();
             return;
         }
 
         String option = args[0];
 
-        switch (option){
+        switch (option) {
             case "-cp":
-                if(args.length<3){
-                optionsNumberErr();
-                return;
+                if (args.length < 3) {
+                    optionsNumberErr();
+                    return;
                 }
-                copyFile(args[1], args[2]);
+                File source = new File(args[1]);
+                File target = new File(args[2]);
+                copy(source, target);
                 break;
             case "-rm":
-                if(args.length<2){
+                if (args.length < 2) {
                     optionsNumberErr();
                     return;
                 }
-                deleteFile(args[1]);
+                delete(args[1]);
                 break;
             case "-mv":
-                if(args.length<3){
+                if (args.length < 3) {
                     optionsNumberErr();
                     return;
                 }
-                copyFile(args[1], args[2]);
-                deleteFile(args[1]);
+                File mSource = new File(args[1]);
+                File mTarget = new File(args[2]);
+                copy(mSource, mTarget);
+                delete(args[1]);
                 break;
-            case "-f":
-                if(args.length<2){
+            case "-touch":
+                if (args.length < 2) {
                     optionsNumberErr();
                     return;
                 }
                 createFile(args[1]);
+                break;
+            case "-mkdir":
+                if (args.length < 2) {
+                    optionsNumberErr();
+                    return;
+                }
+                createDir(args[1]);
                 break;
             default:
                 optionsNumberErr();
@@ -68,7 +62,7 @@ public class MainActivity {
     }
 
 
-    public static void optionsNumberErr(){
+    public static void optionsNumberErr() {
         Options options = new Options();
         HelpFormatter formatter = new HelpFormatter();
 
@@ -88,70 +82,116 @@ public class MainActivity {
         create.setRequired(false);
         options.addOption(create);
 
-        System.out.println("Not enough options!");
-        formatter.printHelp("utility-name", options);
+        Option mkdir = new Option("mkdir", "createDir", true, "create directory");
+        mkdir.setRequired(false);
+        options.addOption(mkdir);
+
+        System.out.println("Not enough or wrong options!");
+        formatter.printHelp("ParseOptions", options);
     }
 
 
-    public static void copyFile (String s, String s2) throws IOException {
-        int num;
-        FileInputStream fileIn;
-        FileOutputStream fileOut;
+    public static void copy(File sourceLocation, File targetLocation) throws IOException {
+        if (sourceLocation.isDirectory()) {
+            copyDirectory(sourceLocation, targetLocation);
+        } else {
+            copyFile(sourceLocation, targetLocation);
+        }
+    }
 
+
+        public static void copyDirectory(File source, File target) throws IOException {
+            if (!target.exists()) {
+                target.mkdir();
+            }
+
+            for (String f : source.list()) {
+                copy(new File(source, f), new File(target, f));
+            }
+        }
+
+        public static void copyFile(File source, File target) throws IOException {
+            try {
+                InputStream in = new FileInputStream(source);
+                OutputStream out = new FileOutputStream(target);
+
+                byte[] buf = new byte[1024];
+                int length;
+                while ((length = in.read(buf)) > 0) {
+                    out.write(buf, 0, length);
+                }
+            } catch (IIOException e) {
+                System.err.println("IIOException" + e);
+                e.printStackTrace();
+                return;
+            }
+        }
+
+
+    public static void createFile(String s) throws IOException{
+        File file = new File(s);
+
+            try {
+                file.createNewFile();
+            } catch (FileNotFoundException e) {
+                System.err.println("FileNotFoundException" + e);
+                return;
+            } catch (IOException e) {
+                System.err.println("IIOException" + e);
+                e.printStackTrace();
+                return;
+            }
+    }
+
+    public static void createDir(String s) {
         try {
-            try {
-                fileIn = new FileInputStream(s);
-            } catch (FileNotFoundException e) {
-                System.out.println("Input File Not Found.");
-                return;
-            }
-            try {
-                fileOut = new FileOutputStream(s2);
-            } catch (FileNotFoundException e) {
-                System.out.println("Error Opening Output File.");
-                return;
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Incorrect argument use:java CopyFile Source Destination");
+            File file = new File(s);
+            file.mkdirs();
+        } catch (NullPointerException e) {
+            System.err.println("NullPointerException" + e);
             return;
         }
-        try {
-            do {
-                num = fileIn.read();
-                if (num != -1) {
-                    fileOut.write(num);
+    }
+
+    public static void delete(String s) {
+        File file = new File(s);
+        if (file.exists()) {
+            if (file.isFile()) {
+                try {
+                    file.delete();
+                } catch (NullPointerException e) {
+                    System.err.println("NullPointerException" + e);
+                    return;
+                }
+            } else if (file.isDirectory()) {
+                if (file.exists()) {
+                    do {
+                        deleteDir(file);
+                    } while (file.exists());
                 }
             }
-            while (num != -1);
-        } catch (IOException e) {
-            System.out.println("File Error: Could not copy file.");
-        }
-
-        fileIn.close();
-        fileOut.close();
-    }
-
-
-    public static void createFile (String s){
-        try {
-            File file = new File(s);
-            file.createNewFile();
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException" + e);
-            return;
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else{
+            System.out.println("File or folder does not exist!");
         }
     }
-    public static void deleteFile (String s){
-        try {
-            File file = new File(s);
+
+    public static void deleteDir(File file) {
+
+        if (file.isDirectory()) {
+            String fileList[] = file.list();
+            if (fileList.length == 0) {
+                file.delete();
+            } else {
+                int size = fileList.length;
+                for (int i = 0; i < size; i++) {
+                    String fileName = fileList[i];
+                    String fullPath = file.getPath() + "/" + fileName;
+                    File fileOrFolder = new File(fullPath);
+                    deleteDir(fileOrFolder);
+                }
+            }
+        } else {
             file.delete();
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException" + e);
-            return;
         }
     }
-
-
-    }
+}
